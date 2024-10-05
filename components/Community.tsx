b260933cbd1b93071a49d589b7f611fd51b4b845
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-05 18:28:19 +0530

    made changes

--- components/Community.tsx
+++ components/Community.tsx
@@ -1,19 +1,36 @@
 "use client";
-import React from "react";
-//import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import Modal from "react-modal";
+import { IoCloseSharp } from "react-icons/io5";
 import Animated from "@/components/Animation";
 
 const CommunitySection = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Animated>
       <section className="py-24 px-6 text-center bg-primary text-primary-foreground">
         <h2 className="text-3xl font-bold mb-6">Join Our Community 🤝</h2>
+
         <p className="text-xl mb-8">Embrace the night. Code with might.</p>
-        <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
+
+        <button
+          onClick={openModal}
+          className="bg-slate-800 no-underline group cursor-pointer relative shadow-xl h-12 shadow-zinc-300 rounded-full text-xs font-semibold leading-6 text-white inline-block"
+        >
           <span className="absolute inset-0 overflow-hidden rounded-full">
-            <span className="absolute inset-0 rounded-full bg-[radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+            <span className="absolute inset-0 rounded-full bg-[radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,1)_0%,rgba(56,189,248,0.6)_50%,rgba(56,189,248,0)_100%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
           </span>
-          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 group-hover:bg-zinc-900 transition-colors duration-500">
+
+          <div className="relative flex space-x-2 items-center z-10 h-5/6 rounded-full bg-zinc-950 py-0.5 px-4 m-1 ring-1 ring-black/10 group-hover:bg-zinc-950 transition-colors duration-500">
             <span>{`Get Started Now`}</span>
             <svg
               width="16"
@@ -32,8 +49,36 @@ const CommunitySection = () => {
               ></path>
             </svg>
           </div>
-          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
         </button>
+
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-background rounded-lg shadow-lg"
+        >
+          <h2 className="text-3xl">Join Us</h2>
+          <IoCloseSharp
+            onClick={closeModal}
+            className="text-lg hover:cursor-pointer absolute top-5 right-5"
+          />
+          <form className="flex flex-col justify-center align-middle gap-5">
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              className="h-10 bg-slate-800 w-56 p-2"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              className="h-10 bg-slate-800 w-56 p-2"
+            />
+            <button type="submit" className="w-40 h-10 bg-slate-800">
+              Submit
+            </button>
+          </form>
+        </Modal>
       </section>
     </Animated>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,7 @@ module.exports = {
         shimmer: "shimmer 2s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": 'spin 10s linear infinite',
       },
     },
   },
