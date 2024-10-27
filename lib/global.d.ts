62fd722d978537ae30c5e081d747f6ea1f31c936
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-27 17:03:16 +0530

    fixed linting related bug in global.d.ts

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -1,5 +1,9 @@
 import { MongoClient } from "mongodb";
 //for mongodb type error handling
+
+/* eslint no-var: "off" */
+/* eslint prefer-const: "off" */
+
 declare global {
     var mongoClientPromise: Promise<MongoClient> | undefined;
 }
