00cf4e16035d2c64f4c58f5b391724350cd266f0
ROHIT8759 <kundurohit53@gmail.com>
2024-11-12 09:18:22 +0530

    Update in Join_Us part & README

--- components/Community.tsx
+++ components/Community.tsx
@@ -38,35 +38,37 @@ const CommunitySection = () => {
               <h2 className="text-6xl py-3 font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">
                 Code by Night, Innovate by Light
               </h2>
-              
-              <p className="text-xl mb-10 text-gray-300">
+
+              <p className="dark:text-slate-400 text-slate-600 text-2xl mb-5">
                 Join the L N C and be part of a vibrant community where
                 creativity thrives after dark. Together, we turn caffeine into
                 code and dreams into digital reality.
               </p>
-              
-              <p className="text-lg mb-10 text-gray-400">
+
+              <p className="dark:text-slate-400 text-slate-600 text-2xl mb-5">
                 Whether you&apos;re a seasoned developer or just starting, we
                 welcome you with open arms. Participate in coding sessions,
                 hackathons, and learn from each other&apos;s experiences.
               </p>
-              
-              <p className="text-lg mb-10 text-gray-400">
+
+              <p className=" dark:text-slate-400 text-slate-600 text-2xl mb-5">
                 Share your projects, seek feedback, and connect with like-minded
                 individuals who are just as passionate about coding as you are.
               </p>
+
+              <button
+                onClick={() => setIsOpen(true)}
+                className="group relative inline-flex items-center justify-center overflow-hidden rounded-full p-0.5 font-bold text-xl"
+              >
+                <span className="absolute inset-0 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 opacity-70 transition-all duration-400 group-hover:opacity-100 group-hover:blur-sm"></span>
+                <span className="relative rounded-full bg-gray-900 px-10 py-6 transition-all duration-400 group-hover:bg-opacity-90">
+                  Join LNC
+                </span>
+              </button>
             </div>
           </div>
 
-          <button
-            onClick={() => setIsOpen(true)}
-            className="group relative inline-flex items-center justify-center overflow-hidden rounded-full p-0.5 font-bold text-xl"
-          >
-            <span className="absolute inset-0 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 opacity-70 transition-all duration-400 group-hover:opacity-100 group-hover:blur-sm"></span>
-            <span className="relative rounded-full bg-gray-900 px-10 py-6 transition-all duration-400 group-hover:bg-opacity-90">
-              Join LNC
-            </span>
-          </button>
+
         </div>
       </section>
       <div className="h-64 bg-gray-900"></div>
@@ -78,7 +80,7 @@ const CommunitySection = () => {
               Become a Night Coder
             </DialogTitle>
           </DialogHeader>
-          <JoinUs/>
+          <JoinUs />
         </DialogContent>
       </Dialog>
     </Animated>
