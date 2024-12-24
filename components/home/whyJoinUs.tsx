fc91fa73b1e5a94d3e505624dae2d67265941b36
Jit Debnath <jit.nathdeb@gmail.com>
2024-12-24 23:05:58 +0530

    removed stars bg

--- components/home/whyJoinUs.tsx
+++ components/home/whyJoinUs.tsx
@@ -5,46 +5,42 @@ import { AnimatedBackground } from "./BackGround";
 function WhyJoinUs() {
   return (
     <AnimatedBackground
-    elementType="star"
+      elementType="star"
       color="rgba(255, 255, 255, 0.7)"
       count={200}
-      >
-    <div className="relative w-full h-[450vh]">
-      <video
-        autoPlay
-        muted
-        loop
-        className="sticky top-0 w-screen h-screen object-cover z-[-1]"
-        src="/videos/stars.mp4"
-      ></video>
-      <div className="z-10 -top-20">
-        <div id="why-join-us" className="flex flex-col space-y-96 px-10 py-96">
-          {whyJoinUs.whyJoinUs?.map((item, index) => (
-            <div
-              key={index}
-              className={
-                index % 2
-                  ? "text-left flex items-start flex-col"
-                  : "text-right flex items-end flex-col"
-              }
-            >
-              <h2
-                className="text-5xl font-semibold text-gray-100 mb-2"
-                style={{ maxWidth: "60ch" }}
+    >
+      <div className="relative w-full h-[450vh]">
+        <div className="z-10 -top-20">
+          <div
+            id="why-join-us"
+            className="flex flex-col space-y-96 px-10 py-96"
+          >
+            {whyJoinUs.whyJoinUs?.map((item, index) => (
+              <div
+                key={index}
+                className={
+                  index % 2
+                    ? "text-left flex items-start flex-col"
+                    : "text-right flex items-end flex-col"
+                }
               >
-                {item.heading}
-              </h2>
-              <p
-                className="text-gray-200 text-2xl"
-                style={{ maxWidth: "60ch" }}
-              >
-                {item.paragraph}
-              </p>
-            </div>
-          ))}
+                <h2
+                  className="text-5xl font-semibold text-gray-100 mb-2"
+                  style={{ maxWidth: "60ch" }}
+                >
+                  {item.heading}
+                </h2>
+                <p
+                  className="text-gray-200 text-2xl"
+                  style={{ maxWidth: "60ch" }}
+                >
+                  {item.paragraph}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </AnimatedBackground>
   );
 }
