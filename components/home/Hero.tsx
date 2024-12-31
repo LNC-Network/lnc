cc3a8ba6daec5b62c857b41e5c639a1b913e6b4d
Jit Debnath <jit.nathdeb@gmail.com>
2024-12-31 19:03:00 +0530

    pushed bg star

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Animated from "@/components/Animation"; // Import your Animated component
 import useFluidAnimation from "@/hooks/useFluidAnimation";
 import ModelView from "./Model";
-// import Image from "next/image";
+import Image from "next/image";
 const Hero: React.FC = () => {
   useFluidAnimation();
   return (
@@ -17,16 +17,27 @@ const Hero: React.FC = () => {
           <div className="absolute z-30">
             <ModelView />
           </div>
-          {/* <div className="relative h-screen w-screen z-0">
+          <div className="relative h-screen w-screen z-0">
             <Image
-              src="/images/giphy.gif"
+              src="/images/bg.gif"
               alt="Fullscreen GIF"
               layout="fill" // Ensure the image fills the parent container
               objectFit="cover" // Ensure the image covers the screen
-              unoptimized // Avoid GIF optimization
               priority // Load the image immediately
             />
-          </div> */}
+          </div>
+          <div className="relative h-screen w-screen z-0">
+            <Image
+              src="/images/bg.gif"
+              alt="Fullscreen GIF"
+              layout="fill" // Ensure the image fills the parent container
+              objectFit="cover" // Ensure the image covers the screen
+              priority // Load the image immediately
+              style={{
+                transform: "scaleY(-1)", // Flip the image vertically
+              }}
+            />
+          </div>
         </section>
       </Animated>
     </>
