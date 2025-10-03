0809120390fdd28f7f2b876e53ae234c1304dc49
Jit Debnath <jit.nathdeb@gmail.com>
2025-10-03 16:12:07 +0530

    edit

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -8,12 +8,16 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const emails = pgTable("emails", {
-  id: serial("id").primaryKey(),
+  id: serial("id").primaryKey(), // auto-incrementing integer primary key
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
-    .notNull(),
+    .notNull(), // auto set to current timestamp on insert
+
+  // ******************Collectable fields******************
   email: text("email").notNull().unique(),
   ipAddress: varchar("ip_address", { length: 255 }),
   userAgent: text("user_agent"),
-  isActive: boolean("is_active").default(true),
+  // *******************************************************
+
+  isActive: boolean("is_active").default(true), // default to true
 });
