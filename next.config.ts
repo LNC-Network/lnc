78ce2996b5725a3b73aad9ef12eaee18bc37fd18
Jit Debnath <jit.nathdeb@gmail.com>
2025-10-15 18:07:19 +0530

    vercel faster build methord turned on

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "assets.hackquest.io" },
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
