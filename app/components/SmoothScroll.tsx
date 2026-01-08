64e840644eb3698b97eb7bf1062e373398e7215b
Jit Debnath <jit.nathdeb@gmail.com>
2026-01-08 17:22:07 +0530

    adwad

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
 
 export default function SmoothScroll({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const lenisRef = useRef<any>(null);
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(update);
+      gsap.ticker.lagSmoothing(500, 33);
+    };
+  }, []);
+
   return (
-    <ReactLenis root>
+    <ReactLenis root ref={lenisRef} autoRaf={false}>
       {children as React.ComponentProps<typeof ReactLenis>["children"]}
     </ReactLenis>
   );
